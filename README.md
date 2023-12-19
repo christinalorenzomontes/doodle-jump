fabdb6b13e1a2bd544e3b5f8a22ab67bed3f9a3c
Christina Lorenzo <christinalorenzo@gmail.com>
2023-12-19 14:11:43 +0100

    add some functionalities

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ Copyright (c) 2020 Ania Kubow &#10084;
 
 ## Description
 
-This project is a fun and exciting JavaScript endeavor aimed at practicing and having a blast with your coding skills. Dive into the world of Doodle Jump and explore the magic of web development through this entertaining project!
+This project is a fun and exciting JavaScript endeavor aimed at practicing and having a blast with coding skills. Dive into the world of Doodle Jump and explore the magic of web development through this entertaining project!
 
 Happy coding! 🚀✨
--- index.js
+++ index.js
@@ -1,3 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const doodler = document.createElement('div');
+    let isGameOver = false;
+    let platformCount = 5;
+    let platforms = [];
+    
+    let doodlerLeftSpace = 50;
+    let doodlerBottomSpace = 150;
+
+    class Platform {
+        constructor(newPlatBottom) {
+            this.left = Math.random() * 315
+            this.bottom = newPlatBottom
+            this.visual = document.createElement('div')
+
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+          }
+    }
+
+    const createPlatforms = () => {
+        for(let i =0; i < platformCount; i++) {
+            let platGap = 600 / platformCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform (newPlatBottom)
+            platforms.push(newPlatform)
+            console.log(platforms)
+        }
+    }
+
+    
+    
+    const createDoodler = () => {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler');
+        doodlerLeftSpace = platforms[0].left;
+        doodler.style.left = doodlerLeftSpace + 'px';
+        doodler.style.bottom = doodlerBottomSpace + 'px';
+    }
+
+    createDoodler();
     
 })
+
